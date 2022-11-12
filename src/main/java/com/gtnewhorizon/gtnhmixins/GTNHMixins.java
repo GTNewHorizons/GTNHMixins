3f89c00ffa94bcdacbc7abfc5ad22087d4a6f811
Jason Mitchell <mitchej@gmail.com>
2022-11-12 09:11:48 -0800

    2.0.2 release

--- src/main/java/com/gtnewhorizon/gtnhmixins/GTNHMixins.java
+++ src/main/java/com/gtnewhorizon/gtnhmixins/GTNHMixins.java
@@ -4,7 +4,7 @@ import cpw.mods.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = GTNHMixins.MODID, version = "2.0.1", name = GTNHMixins.NAME, acceptableRemoteVersions = "*")
+@Mod(modid = GTNHMixins.MODID, version = "2.0.2", name = GTNHMixins.NAME, acceptableRemoteVersions = "*")
 public class GTNHMixins {
     public static final String NAME = "GTNHMixins";
     public static final String MODID = "gtnhmixins";
