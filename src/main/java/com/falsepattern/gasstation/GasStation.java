5152819eaf1b4ef8631b7cf7f07b1c32973378cf
Jason Mitchell <mitchej@gmail.com>
2022-12-29 08:26:01 -0800

    Misc compatibility (#9)
    * Add codechickenlib manual identification
    
    * Skeleton GasStation Mod for limited compat
    
    * bump version

---
+++ src/main/java/com/falsepattern/gasstation/GasStation.java
@@ -0,0 +1,7 @@
+package com.falsepattern.gasstation;
+
+import cpw.mods.fml.common.Mod;
+
+@Deprecated
+@Mod(modid = "gasstation", name = "GasStation", version = "1.0.0", acceptableRemoteVersions = "*")
+public class GasStation { }
